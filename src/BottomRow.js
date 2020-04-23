00d8b8bb45a85dbf00394eadf08066fd0737a6c0
stephanie mairena <stefyem7711@gmail.com>
2020-04-22 21:49:08 -0400

    finalee

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -8,19 +8,19 @@ const BottomRow = () => {
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">UpperCut</h3>
-        <div className="down__value"><span>{ build }</span></div>
+        <div className="down__value"><span>0</span></div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">Shootstyle</h3>
-        <div className="toGo__value"><span>{ build }</span></div>
+        <div className="toGo__value"><span>0</span></div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Grenade</h3>
-        <div className="ballOn__value"><span>{ build }</span></div>
+        <div className="ballOn__value"><span>0</span></div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Explode</h3>
-        <div className="quarter__value"><span>{ build }</span></div>
+        <div className="quarter__value"><span>0</span></div>
       </div>
     </div>
   );
